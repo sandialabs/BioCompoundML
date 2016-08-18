fe7cee44c929e7515e7c2b49384fd39509efc6df
Corey Hudson <cmhudso@sandia.gov>
2016-08-18 12:31:40 -0700

    Fixed feature selection issue

--- bcml/tests/bcml_tests.py
+++ bcml/tests/bcml_tests.py
@@ -99,7 +99,7 @@ class BCMLTests(unittest.TestCase):
         stdoutdata, stderrdata = process.communicate()
         self.assertEqual(process.returncode, 0)
         print("Testing feature selection")
-        args = ['python', _executable, '-i', _data_min, '--train', '--pred', 'RON', '--fingerprint', '--random', '12345', '--selection']
+        args = ['python', _executable, '-i', _data_min, '--train', '--pred', 'RON', '--fingerprint', '--experimental', '--random', '12345', '--selection']
         process = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         stdoutdata, stderrdata = process.communicate()
         self.assertEqual(process.returncode, 0)
